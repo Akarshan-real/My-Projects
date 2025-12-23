bdbaaaa841f73e93260e67021570b60818149687
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-12-23 17:02:42 +0530

    logger edited

--- ToDo/FrontEnd/src/components/LogIn.jsx
+++ ToDo/FrontEnd/src/components/LogIn.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef } from 'react'
+// logger.jsx
+import React, { useState, useRef , useEffect } from 'react'
 import './LogIn.css'
 
 const LogIn = ({ logInChanger, nameCheck }) => {
